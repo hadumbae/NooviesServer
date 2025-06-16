7b8b67bf8af372459116e7d459f6c4bfe5187481
hadumbae <cmcdnn6919@gmail.com>
2025-06-16 14:31:51 +0700

    refactor(common): Update Multr Config
    Update Multr config. Add file size limits, trim the image constant, and remname exported variable.

--- src/shared/constants/ImageTypeConstant.ts
+++ src/shared/constants/ImageTypeConstant.ts
@@ -1,11 +1,6 @@
 export default [
     "image/png",
-    "image/jpg",
     "image/jpeg",
-    "image/gif",
     "image/webp",
     "image/avif",
-    "image/bmp",
-    "image/svg+xml",
-    "image/tiff",
 ] as const;
