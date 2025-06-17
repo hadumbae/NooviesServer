cd4bc1a3a0f12e9bdb0752bff8d3dc1e8b71c002
hadumbae <cmcdnn6919@gmail.com>
2025-06-17 11:41:15 +0700

    refactor(common): Remove unused enums.
    Remove `ZodEnumHelpers.ts` and `ZodImageHelpers.ts`.

--- src/shared/schema/helpers/ZodEnumHelpers.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-import CountryConstant from "../../constants/CountryConstant.js";
-
-export const CountryEnum = z.enum(CountryConstant, {message: "Invalid Country Value."});
-export type CountryEnumType = z.infer<typeof CountryEnum>;
--- src/shared/schema/helpers/ZodImageHelpers.ts
+++
@@ -1,8 +0,0 @@
-import {z} from "zod";
-
-import {RequiredStringSchema} from "../strings/RequiredStringSchema.js";
-
-export const CloudinaryImageObject = z.object({
-    public_id: RequiredStringSchema,
-    secure_url: RequiredStringSchema.url("Must be a valid URL."),
-});
