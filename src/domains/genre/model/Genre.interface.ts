c49f1c6f865e20dc32615526ac71977537d80de1
hadumbae <cmcdnn6919@gmail.com>
2025-08-09 21:53:54 +0700

    docs(genre): Add docs to Genre interface
    Add docs to the Genre interface.

--- src/domains/genre/model/Genre.interface.ts
+++ src/domains/genre/model/Genre.interface.ts
@@ -1,5 +1,16 @@
+/**
+ * Represents a genre entity with a name and description.
+ *
+ * @interface IGenre
+ */
 export default interface IGenre {
+    /**
+     * The name of the genre.
+     */
     name: string;
+
+    /**
+     * A description providing more details about the genre.
+     */
     description: string;
 }
-
