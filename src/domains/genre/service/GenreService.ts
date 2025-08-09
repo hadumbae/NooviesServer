bcb834c7a341febaa81471a1f74af48726698c14
hadumbae <cmcdnn6919@gmail.com>
2025-08-09 11:33:48 +0700

    feat(genre): Update Genre Service
    Update GenreService with new functions to handle unique names.

--- src/domains/genre/service/GenreService.ts
+++ src/domains/genre/service/GenreService.ts
@@ -4,25 +4,59 @@ import type IGenreSubmit from "../schema/interface/IGenreSubmit.js";
 import ZodParseError from "../../../shared/errors/ZodParseError.js";
 import type {ZodIssue} from "zod";
 import createHttpError from "http-errors";
+import mongoose from "mongoose";
+
+type CreateData = { data: IGenreSubmit };
+type UpdateData = CreateData & { genreID: string };
 
 export interface IGenreService {
-    updateGenre({genreID, data}: { genreID: string, data: IGenreSubmit }): Promise<IGenre>;
+    catchDuplicateError(err: unknown): unknown;
+
+    create(params: CreateData): Promise<IGenre>;
+
+    updateUniqueGenre({genreID, data}: UpdateData): Promise<IGenre | null>;
+
+    update({genreID, data}: UpdateData): Promise<IGenre>;
 }
 
-export default class GenreService implements IGenreService{
-    async updateGenre({genreID, data}: { genreID: string, data: IGenreSubmit }): Promise<IGenre> {
-        const genre = await GenreModel.findById(genreID);
-        if (!genre) throw createHttpError(404, "Not found!");
+export default class GenreService implements IGenreService {
+    async create({data}: CreateData): Promise<IGenre> {
+        try {
+            return await GenreModel.create(data);
+        } catch (err: unknown) {
+            throw this.catchDuplicateError(err);
+        }
+    }
 
-        const genreWithName = await GenreModel.findOne({name: data.name, _id: {$ne: genreID}}).lean();
-        if (genreWithName) {
-            const error = {code: "invalid_string", path: ["name"], message: "Must be unique."};
-            throw new ZodParseError({message: "Authentication failed.", errors: [error as ZodIssue]});
+    async updateUniqueGenre({genreID, data}: UpdateData): Promise<IGenre | null> {
+        try {
+            return GenreModel.findByIdAndUpdate(genreID, data);
+        } catch (err: unknown) {
+            throw this.catchDuplicateError(err);
+        }
+    }
+
+    async update(params: UpdateData): Promise<IGenre> {
+        const genre = await this.updateUniqueGenre(params);
+        if (!genre) throw createHttpError(404, "Not found!");
+        return genre;
+    }
+
+    catchDuplicateError(err: unknown): unknown {
+        const mongooseCheck = err instanceof mongoose.Error
+            && err.cause instanceof mongoose.mongo.MongoServerError
+            && err.cause.code === 11000;
+
+        const mongoCheck = err
+            && typeof err === "object"
+            && "code" in err
+            && (err as { code?: number }).code === 11000;
+
+        if (mongooseCheck || mongoCheck) {
+            const errors: ZodIssue[] = [{code: "custom", message: "Name already exists.", path: ["name"]}];
+            return new ZodParseError({message: "Failed to create genre.", errors});
         }
 
-        const updatedGenre = await GenreModel.findByIdAndUpdate({_id: genreID}, data, {new: true}).populate("movies");
-        if (!updatedGenre) throw createHttpError(500, "Error updating genre.");
-
-        return updatedGenre;
+        return err;
     }
 }
