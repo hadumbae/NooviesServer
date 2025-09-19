a7a9c2550ec62a7ac3e2db91ca66b51daa00a846
hadumbae <cmcdnn6919@gmail.com>
2025-09-19 16:35:23 +0700

    feat(Seat): Add query sorting to controller
    Add `fetchURLQuerySorts` to controller.

--- src/domains/seat/controller/SeatController.ts
+++ src/domains/seat/controller/SeatController.ts
@@ -2,12 +2,13 @@ import type {Request, Response} from "express";
 import BaseCRUDController from "../../../shared/controller/base-crud-controller/BaseCRUDController.js";
 import type ISeat from "../model/ISeat.js";
 import SeatQueryOptionService from "../service/SeatQueryOptionService.js";
-import type {FilterQuery} from "mongoose";
+import type {FilterQuery, SortOrder} from "mongoose";
 import type SeatQueryService from "../service/query-service/SeatQueryService.js";
 import type {
     IBaseCRUDController,
     IBaseCRUDControllerConstructor
 } from "../../../shared/controller/base-crud-controller/BaseCRUDController.types.js";
+import type {SeatsByRowSubmitData} from "../schema/seats/Seat.types.js";
 
 export interface ISeatControllerConstructor extends IBaseCRUDControllerConstructor<ISeat> {
     queryService: SeatQueryService;
@@ -34,8 +35,13 @@ export default class SeatController extends BaseCRUDController<ISeat> implements
         return this.optionService.generateMatchFilters(params);
     }
 
+    fetchURLQuerySorts(req: Request): Partial<Record<keyof ISeat, SortOrder>> {
+        const params = this.optionService.fetchQueryParams(req);
+        return this.optionService.generateQuerySorts(params);
+    }
+
     async createSeatsByRow(req: Request, res: Response): Promise<Response> {
-        const data = req.validatedBody;
+        const data = req.validatedBody as SeatsByRowSubmitData;
         const seats = await this.queryService.createByRow(data);
         return res.status(200).json(seats);
     }
