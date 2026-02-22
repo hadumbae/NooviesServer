0596901c022aca58e7417dccbb6dd1fc7785d640
hadumbae <cmcdnn6919@gmail.com>
2026-02-22 15:31:53 +0700

    refactor(Movie): make fetching required movies simpler
    Fetch the movie and throw an error if not found.

--- src/domains/movie/service/fetch-service/MovieFetchService.ts
+++ src/domains/movie/service/fetch-service/MovieFetchService.ts
@@ -3,24 +3,29 @@
  * MovieFetchService.ts
  */
 
-import type { FetchRequiredMovieBySlugParams, FetchRequiredMovieParams } from "./MovieFetchService.types.js";
+import type {FetchRequiredMovieBySlugParams, FetchRequiredMovieParams} from "./MovieFetchService.types.js";
 import MovieModel from "../../model/Movie.model.js";
-import type { MovieDocument } from "../../type/MovieTypes.js";
+import type {MovieDocument} from "../../type/MovieTypes.js";
+import {fetchOrFailQuery} from "../../../../shared/utility/mongoose/fetchOrFailQuery.js";
 
 /**
  * Retrieves a Movie by `_id`, rejecting if not found.
  *
  * @param params - Identifier and query options
  */
-export const fetchRequiredMovie = (
-    { _id, options: { select, lean } = {} }: FetchRequiredMovieParams
+export const fetchRequiredMovie = async (
+    {_id, options: {select, lean} = {}}: FetchRequiredMovieParams
 ): Promise<MovieDocument> => {
     const query = MovieModel.findById(_id)
 
     if (select) query.select(select);
     if (lean) query.lean();
 
-    return query.orFail()
+    return fetchOrFailQuery({
+        query,
+        httpCode: 404,
+        errorMessage: "Movie Not Found.",
+    });
 }
 
 /**
@@ -29,9 +34,9 @@ export const fetchRequiredMovie = (
  * @param params - Slug and query options
  */
 export const fetchRequiredMovieBySlug = (
-    { slug, options: { select, lean } = {} }: FetchRequiredMovieBySlugParams
+    {slug, options: {select, lean} = {}}: FetchRequiredMovieBySlugParams
 ): Promise<MovieDocument> => {
-    const query = MovieModel.findOne({ slug });
+    const query = MovieModel.findOne({slug});
 
     if (select) query.select(select);
     if (lean) query.lean();
