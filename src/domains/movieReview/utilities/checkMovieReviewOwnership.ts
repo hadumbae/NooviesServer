0a44a4e69a03103c86cdc429b5b3d9c59e89a5e3
hadumbae <cmcdnn6919@gmail.com>
2026-02-25 02:46:54 +0700

    feat(MovieReview): update crud writer with update action
    Update crud writer with update action.

---
+++ src/domains/movieReview/utilities/checkMovieReviewOwnership.ts
@@ -0,0 +1,25 @@
+/**
+ * @file MovieReview ownership check utility.
+ * checkMovieReviewOwnership.ts
+ */
+
+import {Types} from "mongoose";
+import {MovieReviewModel} from "../model/MovieReview.model.js";
+
+/**
+ * Parameters for `checkMovieReviewOwnership`
+ * */
+type OwnershipParams = {
+    userID: Types.ObjectId;
+    reviewID: Types.ObjectId;
+};
+
+/**
+ * Verifies whether a MovieReview belongs to the specified user.
+ */
+export async function checkMovieReviewOwnership(
+    {userID, reviewID}: OwnershipParams
+): Promise<boolean> {
+    const review = await MovieReviewModel.findById(reviewID).orFail();
+    return review.user.equals(userID) ?? null;
+}
