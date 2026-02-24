d360fac4e0eb9f58a39c5118f4a991f1f60172c5
hadumbae <cmcdnn6919@gmail.com>
2026-02-25 02:46:34 +0700

    feat(MovieReview): update crud writer with update action
    Update crud writer with update action.

--- src/domains/movieReview/repositories/MovieReviewCRUDWriter.ts
+++ src/domains/movieReview/repositories/MovieReviewCRUDWriter.ts
@@ -5,8 +5,14 @@
 
 import {CRUDWriter} from "../../../shared/repository/operations/CRUDWriter.js";
 import type {MovieReviewSchemaFields} from "../model/MovieReview.types.js";
-import type {MovieReviewCreateActionParams, MovieReviewCRUDWriterMethods} from "./MovieReviewCRUDWriter.types.js";
+import type {
+    MovieReviewCreateActionParams,
+    MovieReviewCRUDWriterMethods,
+    MovieReviewUpdateActionParams
+} from "./MovieReviewCRUDWriter.types.js";
 import {MovieReviewModel} from "../model/MovieReview.model.js";
+import {checkMovieReviewOwnership} from "../utilities/checkMovieReviewOwnership.js";
+import createHttpError from "http-errors";
 
 /**
  * Write operations for MovieReview.
@@ -24,5 +30,14 @@ export class MovieReviewCRUDWriter
     ): Promise<MovieReviewSchemaFields> {
         const createParams = {...data, user: userID};
         return super.createAction({data: createParams, options});
+    }
+
+    async updateAction(params: MovieReviewUpdateActionParams) {
+        const {_id: reviewID, userID, ...rest} = params;
+
+        const isSameUser = await checkMovieReviewOwnership({userID, reviewID});
+        if (!isSameUser) throw createHttpError(403, "Invalid User, Can Only Update Owned Reviews.");
+
+        return super.updateAction({_id: reviewID, ...rest});
     }
 }
--- src/domains/movieReview/repositories/MovieReviewCRUDWriter.types.ts
+++ src/domains/movieReview/repositories/MovieReviewCRUDWriter.types.ts
@@ -1,15 +1,20 @@
 /**
- * @file Types for MovieReview CRUD writer.
+ * @file MovieReview write-layer type contracts.
  * MovieReviewCRUDWriter.types.ts
  */
 
 import type { MovieReviewCreateInputData } from "../schema/MovieReviewCreateInputSchema.js";
 import { Types } from "mongoose";
 import type { MovieReviewSchemaFields } from "../model/MovieReview.types.js";
-import type {CRUDCreateParams, WriteMethods} from "../../../shared/repository/operations/CRUDWriter.types.js";
+import type {
+    CRUDCreateParams,
+    CRUDUpdateActionParams,
+    WriteMethods,
+} from "../../../shared/repository/operations/CRUDWriter.types.js";
+import type { MovieReviewUpdateInputData } from "../schema/MovieReviewUpdateInputSchema.js";
 
 /**
- * Parameters for creating a MovieReview.
+ * Parameters for MovieReview create operations.
  */
 export type MovieReviewCreateActionParams = Pick<CRUDCreateParams, "options"> & {
     userID: Types.ObjectId;
@@ -17,8 +22,17 @@ export type MovieReviewCreateActionParams = Pick<CRUDCreateParams, "options"> & 
 };
 
 /**
- * Write operations contract for MovieReview.
+ * Parameters for MovieReview update operations.
  */
-export interface MovieReviewCRUDWriterMethods extends WriteMethods<MovieReviewSchemaFields>{
+export type MovieReviewUpdateActionParams = Omit<CRUDUpdateActionParams, "data"> & {
+    userID: Types.ObjectId;
+    data: MovieReviewUpdateInputData;
+};
+
+/**
+ * Write operation contract for MovieReview.
+ */
+export interface MovieReviewCRUDWriterMethods extends WriteMethods<MovieReviewSchemaFields> {
     createAction(params: MovieReviewCreateActionParams): Promise<MovieReviewSchemaFields>;
+    updateAction(params: MovieReviewUpdateActionParams): Promise<MovieReviewSchemaFields>;
 }
