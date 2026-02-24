801bc6c3c36d50dacc323a9ff0844a57d901c21c
hadumbae <cmcdnn6919@gmail.com>
2026-02-24 16:39:31 +0700

    refactor(MovieReview): create model
    Create `MovieReview` model.

--- src/domains/movieReview/model/MovieReview.types.ts
+++ src/domains/movieReview/model/MovieReview.types.ts
@@ -9,6 +9,7 @@ import { Types } from "mongoose";
  * Field shape for MovieReview documents.
  */
 export interface MovieReviewSchemaFields {
+    _id: Types.ObjectId;
     user: Types.ObjectId;
     movie: Types.ObjectId;
     rating: number;
