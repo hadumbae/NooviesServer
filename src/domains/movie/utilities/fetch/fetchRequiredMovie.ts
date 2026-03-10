6e3d19f7d52b9505c13c79b6354ecfcbc4ba786c
hadumbae <cmcdnn6919@gmail.com>
2026-03-11 03:00:50 +0700

    feat: create function to fetch required movies by ID or by slug

---
+++ src/domains/movie/utilities/fetch/fetchRequiredMovie.ts
@@ -0,0 +1,41 @@
+/**
+ * @file Fetches a required movie by identifier with optional population settings.
+ * @filename fetchRequiredMovie.ts
+ */
+
+import type {RequestOptions} from "../../../../shared/types/request-options/RequestOptions.js";
+import {Types} from "mongoose";
+import populateQuery from "../../../../shared/utility/mongoose/populateQuery.js";
+import MovieModel from "../../model/Movie.model.js";
+import type {SlugString} from "../../../../shared/schema/strings/SlugStringSchema.js";
+import type {MovieDocument} from "../../type/MovieTypes.js";
+
+/**
+ * Parameters for fetching a required movie.
+ * Accepts either a database identifier or a slug.
+ */
+type FetchParams = Omit<RequestOptions, "limit"> & (
+    /** Fetch by MongoDB identifier */
+    | { _id: Types.ObjectId, slug?: never }
+    /** Fetch by unique slug */
+    | { _id?: never, slug: SlugString }
+    );
+
+/**
+ * Fetches a movie with optional population and virtual configuration.
+ * Throws if no document is found.
+ */
+export function fetchRequiredMovie(
+    {_id, slug, populate, virtuals, populatePaths}: FetchParams,
+): Promise<MovieDocument> {
+    const baseQuery = _id
+        ? MovieModel.findById(_id)
+        : MovieModel.findOne({slug});
+
+    const query = populateQuery({
+        query: baseQuery,
+        options: {populate, virtuals, populatePaths},
+    });
+
+    return query.orFail();
+}
