2d26e8263fd55be152bfbf6a1fafe77a592e8454
hadumbae <cmcdnn6919@gmail.com>
2026-03-12 00:57:21 +0700

    fix: incorrect request params in `BrowseMovieCreditsController`

--- src/domains/movieCredit/controllers/client/BrowseMovieCreditsController.ts
+++ src/domains/movieCredit/controllers/client/BrowseMovieCreditsController.ts
@@ -7,6 +7,7 @@ import type {ControllerAsyncFunc} from "../../../../shared/types/ControllerTypes
 import type {Request, Response} from "express";
 import {fetchRequiredMovie} from "../../../movie/utilities/fetch/fetchRequiredMovie.js";
 import {fetchCreditsForMovie} from "../../services/credits-for-movie/CreditsForMovieService.js";
+import {MoviePopulatePaths} from "../../../movie/queries/MoviePopulatePaths.js";
 
 /**
  * Fetches a movie and its grouped cast and crew credits for browse views.
@@ -14,9 +15,16 @@ import {fetchCreditsForMovie} from "../../services/credits-for-movie/CreditsForM
 export const getFetchGroupedCreditsWithMovie: ControllerAsyncFunc = async (
     req: Request, res: Response
 ) => {
-    const {movieSlug} = req.params;
+    const {slug} = req.params;
 
-    const movie = await fetchRequiredMovie({slug: movieSlug});
+    const movie = await fetchRequiredMovie({
+        slug, options: {
+            populate: true,
+            virtuals: true,
+            populatePaths: MoviePopulatePaths
+        }
+    });
+
     const data = await fetchCreditsForMovie(movie._id);
 
     return res
