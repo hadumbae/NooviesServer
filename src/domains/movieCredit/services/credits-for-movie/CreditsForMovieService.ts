654f99a736e104cb70416056f79ada21924ac2ac
hadumbae <cmcdnn6919@gmail.com>
2026-03-12 00:53:59 +0700

    fix: correct pipelines for `fetchCreditsForMovie` function

--- src/domains/movieCredit/services/credits-for-movie/CreditsForMovieService.ts
+++ src/domains/movieCredit/services/credits-for-movie/CreditsForMovieService.ts
@@ -15,8 +15,8 @@ export const fetchCreditsForMovie = async (
 ): Promise<GroupedCreditsForMovieData> => {
     const [data] = await MovieCredit.aggregate<GroupedCreditsForMovieData>([
         {$match: {movie: movieID}},
-        {$lookup: {from: "roletypes", localField: "roletype", foreignField: "_id", as: "roleType"}},
-        {$lookup: {from: "persons", localField: "person", foreignField: "_id", as: "person"}},
+        {$lookup: {from: "roletypes", localField: "roleType", foreignField: "_id", as: "roleType"}},
+        {$lookup: {from: "people", localField: "person", foreignField: "_id", as: "person"}},
         {$unwind: "$roleType"},
         {$unwind: "$person"},
         {
@@ -24,15 +24,6 @@ export const fetchCreditsForMovie = async (
                 castCredits: [
                     {$match: {department: "CAST"}},
                     {$sort: {billingOrder: 1, isPrimary: 1, characterName: 1}},
-                    {
-                        $group: {
-                            _id: "$roleType.category",
-                            category: {$first: "$roleType.category"},
-                            credits: {$push: "$$ROOT"},
-                            totalCredits: {$sum: 1},
-                        },
-                    },
-                    {$project: {_id: 0, category: 1, credits: 1}},
                 ],
                 crewCredits: [
                     {$match: {department: "CREW"}},
@@ -44,7 +35,7 @@ export const fetchCreditsForMovie = async (
                             totalCredits: {$sum: 1},
                         },
                     },
-                    {$project: {_id: 0, category: 1, credits: 1}},
+                    {$project: {_id: 0, category: 1, credits: 1, totalCredits: 1}},
                 ]
             },
         },
