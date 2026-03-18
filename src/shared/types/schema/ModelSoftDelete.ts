be38fff8a1cbd6612bd80e8a4c68dbb69476424e
hadumbae <cmcdnn6919@gmail.com>
2026-03-19 00:37:26 +0700

    feat: add `deletedAt` to soft delete

--- src/shared/types/schema/ModelSoftDelete.ts
+++ src/shared/types/schema/ModelSoftDelete.ts
@@ -4,11 +4,16 @@
  */
 
 /**
- * Common flag for models implementing soft-deletion.
+ * Common flags for models implementing soft-deletion.
  */
 export type ModelSoftDelete = {
     /** * Indicates if the record is hidden from standard queries.
      * Typically matches {@link IsDeletedSchemaTypeOptions}.
      */
-    isDeleted?: boolean;
+    isDeleted: boolean;
+
+    /** * Audit timestamp via {@link DeletedAtSchemaTypeOptions}.
+     * Used for restoration or TTL-based permanent cleanup.
+     */
+    deletedAt: Date | null;
 }
