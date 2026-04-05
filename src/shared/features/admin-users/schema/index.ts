fea07950a8715ee5629cdac9b6ee6ddbde0316c3
hadumbae <cmcdnn6919@gmail.com>
2026-04-05 21:49:14 +0700

    feat: create schemas for moderation messages

---
+++ src/shared/features/admin-users/schema/index.ts
@@ -0,0 +1,19 @@
+import {
+    type AdminModerationMessage,
+    AdminModerationMessageSchema
+} from "@shared/features/admin-users/schema/AdminModerationMessageSchema";
+import {
+    type AdminModerationMessageInputData,
+    AdminModerationMessageInputSchema
+} from "@shared/features/admin-users/schema/AdminModerationMessageInputSchema";
+
+
+export {
+    AdminModerationMessageSchema,
+    AdminModerationMessageInputSchema,
+}
+
+export type {
+    AdminModerationMessage,
+    AdminModerationMessageInputData,
+}
