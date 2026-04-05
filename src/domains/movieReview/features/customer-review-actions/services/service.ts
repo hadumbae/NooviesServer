ce4d0e811e62bc060fbc29ead71609fa58442694
hadumbae <cmcdnn6919@gmail.com>
2026-04-05 21:36:06 +0700

    feat: scaffold customer movie review action service

---
+++ src/domains/movieReview/features/customer-review-actions/services/service.ts
@@ -0,0 +1,6 @@
+// src/domains/movieReview/features/customer-review-actions/services/service.ts
+
+// Toggle Publicity
+// Reset Display Name
+// Reset Likes
+// Set Ratings
---
+++ src/domains/movieReview/features/customer-review-actions/services/service.types.ts
@@ -0,0 +1,6 @@
+// src/domains/movieReview/features/customer-review-actions/services/service.types.ts
+
+// Toggle Publicity
+// Reset Display Name
+// Reset Likes
+// Set Ratings
