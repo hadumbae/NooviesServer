f671a2ea3d6b8a9a6c1dfb02b3a1e342f8ea0463
hadumbae <cmcdnn6919@gmail.com>
2026-04-05 22:41:25 +0700

    feat: add function to toggle movie review publicity

--- src/domains/movieReview/features/customer-review-actions/services/service.ts
+++ src/domains/movieReview/features/customer-review-actions/services/service.ts
@@ -1,6 +1,49 @@
 // src/domains/movieReview/features/customer-review-actions/services/service.ts
 
 // Toggle Publicity
+
+import type {
+    ToggleReviewPublicityConfig
+} from "@domains/movieReview/features/customer-review-actions/services/service.types";
+import {MovieReview} from "@domains/movieReview/model/MovieReview.model";
+import createHttpError from "http-errors";
+import {MovieReviewModerationLog} from "@domains/movieReview/model/moderationLogs/MovieReviewModerationLog.model";
+import type {MovieReviewSchemaFields} from "@domains/movieReview/model/MovieReview.types";
+
+const toggleReviewPublicity = async (
+    {adminID, reviewID, message}: ToggleReviewPublicityConfig
+): Promise<MovieReviewSchemaFields> => {
+    const review = await MovieReview.findById(reviewID);
+
+    if (!review) {
+        throw createHttpError(404, "Failed to toggle publicity. Movie Review not found.");
+    }
+
+    const log = new MovieReviewModerationLog({
+        action: "MOD_TOGGLE_PUBLICITY",
+        admin: adminID,
+        modDate: new Date(),
+        message,
+    });
+
+    try {
+        await log.validate();
+    } catch (error: unknown) {
+        console.error("Failed to write log.");
+        console.error("Error: ", error);
+
+        throw createHttpError(500, "Failed to toggle publicity. Please try again later.");
+    }
+
+    review.isPublic = !review.isPublic;
+    review.moderationLogs.push(log);
+    await review.save();
+
+    return review;
+}
+
 // Reset Display Name
+
 // Reset Likes
+
 // Set Ratings
--- src/domains/movieReview/features/customer-review-actions/services/service.types.ts
+++ src/domains/movieReview/features/customer-review-actions/services/service.types.ts
@@ -1,6 +1,18 @@
 // src/domains/movieReview/features/customer-review-actions/services/service.types.ts
 
 // Toggle Publicity
+
+import {Types} from "mongoose";
+import type {AdminModerationMessage} from "@shared/features/admin-users/schema";
+
+export type ToggleReviewPublicityConfig = {
+    adminID: Types.ObjectId;
+    reviewID: Types.ObjectId;
+    message: AdminModerationMessage;
+}
+
 // Reset Display Name
+
 // Reset Likes
+
 // Set Ratings
