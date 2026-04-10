9dd248501ed4e86f3bb4d5979e2081055625f2c4
hadumbae <cmcdnn6919@gmail.com>
2026-04-11 02:47:31 +0700

    feat: create util to fetch required pagination values

--- src/shared/features/fetch-request-options/utils/index.ts
+++ src/shared/features/fetch-request-options/utils/index.ts
@@ -1,5 +1,9 @@
 import {fetchRequestOptions} from "@shared/features/fetch-request-options/utils/fetchRequestOptions";
+import {
+    fetchRequestPaginationOptions
+} from "@shared/features/fetch-request-options/utils/fetchRequestPaginationOptions";
 
 export {
-    fetchRequestOptions
+    fetchRequestOptions,
+    fetchRequestPaginationOptions,
 }
