b3401776e4f75f3d175a084e663f113da278aa0a
hadumbae <cmcdnn6919@gmail.com>
2026-04-23 23:27:07 +0700

    feat: create population paths for showings

---
+++ src/domains/showing/_feat/query-population/index.ts
@@ -0,0 +1,7 @@
+import {ShowingPopulationPaths} from "@domains/showing/_feat/query-population/ShowingPopulationPaths";
+import {ShowingVirtualPopulationPaths} from "@domains/showing/_feat/query-population/ShowingVirtualPopulationPaths";
+
+export {
+    ShowingPopulationPaths,
+    ShowingVirtualPopulationPaths,
+}
