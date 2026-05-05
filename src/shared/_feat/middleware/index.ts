421cca2d365644b73faf28af1d86960534e94332
hadumbae <cmcdnn6919@gmail.com>
2026-05-06 02:39:31 +0700

    feat: create pipeline schema transformers

--- src/shared/_feat/middleware/index.ts
+++ src/shared/_feat/middleware/index.ts
@@ -3,6 +3,7 @@ import {parseRouteParams} from "@shared/_feat/middleware/parseRouteParams";
 import {buildUnsetFields} from "@shared/_feat/middleware/buildUnsetFields";
 import {parseQuerySortStage} from "@shared/_feat/middleware/parseQuerySortStage";
 import {parseQueryMatchStage} from "@shared/_feat/middleware/parseQueryMatchStage";
+import {buildAuthCRUDQueryMiddleware} from "@shared/_feat/middleware/buildAuthCRUDQueryMiddleware";
 
 
 export {
@@ -11,5 +12,6 @@ export {
     buildUnsetFields,
     parseQueryMatchStage,
     parseQuerySortStage,
+    buildAuthCRUDQueryMiddleware,
 }
 
